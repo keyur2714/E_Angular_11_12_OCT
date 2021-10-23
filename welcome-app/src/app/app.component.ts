fbafaf30e98c12a12682e2dc4b686fac7699ae5e
keyur2714 <keyurjava27@gmail.com>
2021-10-23 15:57:51 +0530

    Created welcome-app

---
+++ welcome-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  message : string = 'Hello Radhe Krishna...!'; // instance variable
+
+  name : string = "Keyur";
+  age : number = 35;
+  city : string = "surat";
+  mobileNo : string = "7387029671";
+  
+
+}
